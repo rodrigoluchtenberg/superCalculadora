21eeeeafd426c6ba171d8e851d76d1fdea244f5f
RODRIGO LUCHTENBERG <rodrigo.luchtenberg@alunos.sc.senac.br>
2024-10-28 21:57:41 -0300

    Adicionando pasta Models e suas propriedades

---
+++ superCalculadora/Models/Operacoes.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperCalculadora.Modelos
+{
+    public class OperacaoDTO
+    {
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+        public string Operacao { get; set; }
+    }
+}
