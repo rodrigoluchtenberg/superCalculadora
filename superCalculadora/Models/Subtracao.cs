81de3463cae712bebbf10d00884f6ceed3cb0a3b
GABRIEL HERON DA CUNHA <gabriel.cunha7@alunos.sc.senac.br>
2024-10-30 20:37:35 -0300

    Adicionando arquivo modificado: Subtracao.cs

--- superCalculadora/Models/Subtracao.cs
+++ superCalculadora/Models/Subtracao.cs
@@ -2,6 +2,9 @@
 {
     public class Subtracao : IOperacao
     {
-
+        public double Calcular(double numero1, double numero2)
+        {
+            return numero1 - numero2;
+        }
     }
 }
