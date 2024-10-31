ec2cea0a206c28f986cd034edb8d3b0964a26880
lucas-bretzke <lucasbretzke@gmail.com>
2024-10-30 21:01:26 -0300

    added Divisao.cs

--- superCalculadora/Models/Divisao.cs
+++ superCalculadora/Models/Divisao.cs
@@ -1,7 +1,16 @@
-﻿namespace SuperCalculadora.Modelos
+﻿using System.Data;
+
+namespace SuperCalculadora.Modelos
 {
     public class Divisao : IOperacao
     {
-
+        public double Calcular(double numero1, double numero2)
+        {
+            if (numero2 == 0)
+            {
+                throw new DivideByZeroException("Não pode dividir por  zero");
+            }
+            return numero1 / numero2;
+        }
     }
 }
